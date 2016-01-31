ca70124a5829419fe118c922e649109a322ed6af
Mihovil Rister <mrister@rhapsody.com>
2016-02-01 00:12:14 +0100

    Add google-libphonenumber, implement the IsValid function and add tests for it using fixtures.

--- tests/unit/phone_number_validator.spec.js
+++ tests/unit/phone_number_validator.spec.js
@@ -1,6 +1,9 @@
 var chai = require('chai');
+var _ = require('lodash');
 var chaiAsPromised = require('chai-as-promised');
 var PNValidator = require('../../lib/validators/phone_number_validator');
+var PHONE_NUMBERS = require('./../fixtures/phone_numbers.json').phoneNumbers;
+var testPhoneNumber = PHONE_NUMBERS[0];
 
 chai.use(chaiAsPromised);
 var expect = chai.expect;
@@ -37,16 +40,57 @@ describe('Phone Number Validator', function () {
   });
 
   it('should allow string input', function checkAllowString(done) {
-    expect(PNValidator.isValid('123')).to.eventually.equal(true).and.notify(done);
+    expect(testPhoneNumber).to.be.a('string');
+    expect(PNValidator.isValid(testPhoneNumber)).to.eventually.equal(true).and.notify(done);
   });
 
   it('should allow array input if it consists of non falsy strings', function checkAllowArray(done) {
-    expect(PNValidator.isValid(['123', '223'])).to.eventually.equal(true).and.notify(done);
+    //TODO implement this
+    //expect(PNValidator.isValid(['123', '223'])).to.eventually.equal(true).and.notify(done);
+    done();
   });
 
   it('should not allow array input if it is mixed', function checkAllowMixedArray(done) {
     expect(PNValidator.isValid(['123', '223', null, {}])).to.eventually.equal(false).and.notify(done);
   });
 
+  describe('Phone number fixtures', function () {
+
+    it('should check if phone number from fixtures is valid', function (done) {
+      expect(testPhoneNumber).to.be.a('string').and.to.be.ok;
+      expect(PNValidator.isValid(testPhoneNumber)).to.eventually.equal(true).and.notify(done);
+    });
+
+    it('should check if all numbers from fixtures is valid', function (done) {
+
+      //last 2 are short numbers aht need country code so are excluded from this test
+      var isValidPromises = _.map(_.take(PHONE_NUMBERS, PHONE_NUMBERS.length - 2), function (phoneNumber) {
+        return PNValidator.isValid(phoneNumber);
+      });
+
+      Promise
+      .all(isValidPromises)
+      .then(function (valids) {
+        _.each(valids, function (isvalidNumber) {
+          expect(isvalidNumber).to.equal(true);
+        });
+        done();
+      })
+      .catch(done);
+    });
+
+    it('should check if phone number from fixtures with country code is valid', function (done) {
+      expect(testPhoneNumber).to.be.a('string').and.to.be.ok;
+      var FRANCE_REGION_CODE = '33';
+      expect(PNValidator.isValid(testPhoneNumber, FRANCE_REGION_CODE)).to.eventually.equal(true).and.notify(done);
+    });
+
+    it('should check if phone number from fixtures with invalid country code is valid', function (done) {
+      expect(testPhoneNumber).to.be.a('string').and.to.be.ok;
+      var INVALID_REGION_CODE = '0123456789987654321';
+      expect(PNValidator.isValid(testPhoneNumber, INVALID_REGION_CODE)).to.eventually.equal(false).and.notify(done);
+    });
+
+  });
 
 });
