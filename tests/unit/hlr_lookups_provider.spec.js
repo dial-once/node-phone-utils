09bfbdf478e64388c0ab81a0f8e0f6d9effb27c8
Mihovil Rister <mrister@rhapsody.com>
2016-02-20 00:45:38 +0100

    Initial hlr lookup implementation with basic tests (hlr-lookups.com and smsapi.com)

--- tests/unit/hlr_lookups_provider.spec.js
+++ tests/unit/hlr_lookups_provider.spec.js
@@ -3,33 +3,23 @@
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 var _ = require('lodash');
-var HLRLookupsProvider = require('../../lib/providers/hlr_lookups_provider');
+var phoneUtils = require('../../lib');
 var BaseProvider = require('../../lib/providers/base_provider');
-
-var winston = require('winston');
-var winstonLogger = new winston.Logger({
-  transports: [
-    new (winston.transports.Console)({level: 'debug'})
-  ]
-});
+var HLRLookupsProvider = phoneUtils.providers.hlrLookups ;
 
 chai.use(chaiAsPromised);
 var expect = chai.expect;
 
 describe('HLR Lookups Provider', function () {
 
-  it ('should work :-)', function (done) {
-    console.log(HLRLookupsProvider);
-    console.log(HLRLookupsProvider.isValid());
-    console.log(HLRLookupsProvider.hlrLookup('test'));
+  it ('should  do HLR Lookup', function (done) {
     expect(HLRLookupsProvider.name).to.be.a('string').and.to.be.ok;
     expect(HLRLookupsProvider).to.be.instanceOf(BaseProvider);
     expect(HLRLookupsProvider.isValid()).to.be.true;
-   // expect(HLRLookupsProvider.hlrLookup()).to.be.instanceOf(Promise);
     HLRLookupsProvider
     .hlrLookup('+491788735001')
-      .then(function (response){
-        console.log(response);
+      .then(function (result){
+        expect(result).to.be.an('object').and.to.be.ok;
         done();
       })
       .catch(done);
