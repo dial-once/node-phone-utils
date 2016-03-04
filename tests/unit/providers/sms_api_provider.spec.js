28dc20ac7955273e5511e7a5b33294b90404f93a
Mihovil Rister <mihovil.rister@gmail.com>
2016-03-04 23:26:20 +0100

    Add phone number validation to smsapi.com provider and tests for it.

--- tests/unit/providers/sms_api_provider.spec.js
+++ tests/unit/providers/sms_api_provider.spec.js
@@ -11,44 +11,73 @@ chai.use(chaiAsPromised);
 var expect = chai.expect;
 var testNumber = '+31612969525';
 
-describe('SMAPI.COM HLR Lookups Provider', function () {
+describe('SMSAPI.com HLR Lookup Provider', function () {
 
-  it ('should perform HLR Lookup', function (done) {
+  var testFailedLookup = function testFailedLookup(wrongNumber, done) {
+    SMSAPILookupsProvider
+    .hlrLookup(wrongNumber)
+    .then(function (results) {
+      done(new Error('Ooops we got results!'));
+    })
+    .catch(function (err) {
+      expect(err).to.be.instanceOf(Error);
+      done();
+    });
+
+  };
+
+  it('should perform HLR Lookup', function (done) {
     expect(SMSAPILookupsProvider.name).to.be.a('string').and.to.be.ok;
     expect(SMSAPILookupsProvider).to.be.instanceOf(BaseProvider);
     expect(SMSAPILookupsProvider.isValid()).to.be.true;
 
     SMSAPILookupsProvider
     .hlrLookup(testNumber)
-      .then(function (results){
-        expect(results).to.be.an('Array').and.to.be.ok;
-        results.forEach(function (result){
-          expect(result).to.be.an('Object').and.to.be.ok;
-        });
-        done();
-      })
-      .catch(function (err){
-        expect(err).to.be.instanceOf(Error);
-        done(err);
+    .then(function (results) {
+      expect(results).to.be.an('Array').and.to.be.ok;
+      results.forEach(function (result) {
+        expect(result).to.be.an('Object').and.to.be.ok;
       });
+      done();
+    })
+    .catch(function (err) {
+      expect(err).to.be.instanceOf(Error);
+      done(err);
+    });
   });
 
-  it ('should not allow itself to be created without valid name', function () {
-    var fn = function() {
+  it('should not perform lookup if no number is supplied', function (done) {
+    testFailedLookup('', done);
+  });
+
+  it('should not perform lookup if  object is supplied', function (done) {
+    testFailedLookup({}, done);
+  });
+
+  it('should not perform lookup if Array is supplied', function (done) {
+    testFailedLookup([testNumber], done);
+  });
+
+  it('should not perform lookup if non E164 formatted number is supplied', function (done) {
+    testFailedLookup('33 55 890 098', done);
+  });
+
+  it('should not allow itself to be created without valid name', function () {
+    var fn = function () {
       return new SMSAPILookupsProviderBase();
     };
     expect(fn).to.throw(Error).and.to.have.property('message', 'Name must be specified');
   });
 
-  it ('should not allow itself to be created without valid username', function () {
-    var fn = function() {
+  it('should not allow itself to be created without valid username', function () {
+    var fn = function () {
       return new SMSAPILookupsProviderBase('testName');
     };
     expect(fn).to.throw(Error).and.to.have.property('message', 'Username must be specified');
   });
 
-  it ('should not allow itself to be created without valid password', function () {
-    var fn = function() {
+  it('should not allow itself to be created without valid password', function () {
+    var fn = function () {
       return new SMSAPILookupsProviderBase('testName', 'testUsername');
     };
     expect(fn).to.throw(Error).and.to.have.property('message', 'Password must be specified');
